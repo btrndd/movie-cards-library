59ec75d5e0cb63dfa91528cbfa0905da66159f13
Bruno Trindade <brunotrndd@gmail.com>
2021-09-04 16:51:11 -0300

    Migrating from trybe repository to my github acc

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main className="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieList;
